232328eaf8b9f306ac29a1755e07409614bf0511
Dollypee48 <akinolad22@gmail.com>
2025-07-16 14:00:00 +0100

    rearrange the server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,29 +3,33 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
+const corsOptions = require("./config/corsOptions");
 const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 
 
+
+require("dotenv").config();
+
 dotenv.config();
+connectDB();
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.send("📚 BookBase API is running");
 });
 
-// Connect to MongoDB and start server
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
-connectDB();
+
 
 mongoose.connection.once("open", () => {
   console.log("✅ Connected to MongoDB");
