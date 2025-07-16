22356277f7456b8b9c2f74ca590a175ebc4bbbe3
Dollypee48 <akinolad22@gmail.com>
2025-07-16 14:14:58 +0100

    change the url

--- frontEnd/src/utils/api.js
+++ frontEnd/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "https://book-base-swd1.vercel.app/api";
+const API = import.meta.env.VITE_API_URL || "https://book-base-swd1.vercel.app";
 
 const api = axios.create({
   baseURL: API,
